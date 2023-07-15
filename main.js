cc267d669524b7ae1d809d4ce6cff1428d2d9ea7
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-07-15 15:51:43 -0700

    Get fetch to work

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log("connected")
+// http://acnhapi.com/v1/villagers
+
+
+// Elements
+const selectVillager = document.querySelector("#villagers")
+
+// Function Calls
+getVillager()
+// villagerOptions()
+
+// Fetching villager data
+function getVillager(){
+    fetch("http://acnhapi.com/v1/villagers")
+    .then(res => res.json())
+    .then(villagers => villagerOptions(Object.values(villagers)))
+    .catch()
+}
+
+//All villagers data
+function villagerOptions(villagers) {
+   for (const villager of villagers){
+    const option = document.createElement("option")
+    option.value = villager.name["name-USen"]
+    option.textContent = villager.name["name-USen"]
+    selectVillager.append(option)
+    console.log(villager.name["name-USen"])
+   }
+}
+
+
+
--- scratch.md
+++ scratch.md
@@ -8,7 +8,7 @@ PHASE 1 PROJECT NOTES
 1.[] HTML CSS JS
 2.[] Public API or db.json
 3.[] 2 distint event listener
-4.[] iterating through arrays
+4.[] Iterating through arrays
 5.[] Use array methods
 6.[] Single page only
 7.[] 30+ commits
