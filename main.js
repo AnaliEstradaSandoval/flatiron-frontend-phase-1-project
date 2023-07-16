7d871d50449201f6c146f65f64b249535a361cce
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-07-15 23:04:11 -0700

    adds button to info div

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Elements
 const selectVillager = document.querySelector(".villagers")
 const villagerContainer = document.querySelector(".villagerinfo-container")
+const button = document.querySelector(".button")
 
 // Function Calls
 getVillager()
@@ -8,7 +9,6 @@ getVillager()
 
 // Event Listeners
 selectVillager.addEventListener("change", getVillagersInfo)
-
 
 
 // Fetching villager data
@@ -45,28 +45,76 @@ function getVillagersInfo(e){
 // Render info on website
 function renderVillagersInfo(villagers){
     villagerContainer.replaceChildren()
+
    renderVillagerCard(villagers)
 }
 
 function renderVillagerCard(villager){
     const villagerName = villager.name["name-USen"]
-    const {birthday, gender, hobby, personality, saying, species, 
-        ["catch-phrase"]: catchPhrase,
-        ["icon_uri"]: iconURI,
-        ["image_uri"]: imageURI
-    } = villager
-   const cardDiv = document.createElement("div")
-   cardDiv.classList.add("card")
 
-   //add event listener to card
+    const {id, ["image_uri"]: imageURI} = villager
+
+    const cardDiv = document.createElement("div")
+
+    cardDiv.classList.add("card")
+   //event listener
+   button.addEventListener("click", e => getVillagerDetails(e, id))
+  
     const image = document.createElement("img")
     image.src = imageURI
 
-    const title = document.createElement("h3")
+    const title = document.createElement("h2")
     title.textContent = villagerName
 
-    cardDiv.append(image, title)
+    cardDiv.append(image, title, button)
     villagerContainer.append(cardDiv)
 }
 
+function getVillagerDetails(e, id){
+    fetch(`http://acnhapi.com/v1/villagers/${id}`)
+    .then(res => res.json())
+    .then(villager => renderVillagerDetails(villager))
+    .catch(error => alert(error))
+}
 
+function renderVillagerDetails(villager){
+    const villagerName = villager.name["name-USen"]
+    const {id, birthday, gender, hobby, personality, saying, species, 
+        ["catch-phrase"]: catchPhrase,
+        ["icon_uri"]: iconURI
+    }= villager
+    console.log(villager)
+
+    // Title Area
+    const title = document.createElement("p")
+    title.textContent = `Villager Name: ${villagerName}`
+    title.style.textDecoration = "underline"
+    let titleArea = document.querySelector(".villager-title")
+    titleArea.replaceChildren()
+    titleArea.append(title)
+
+    // Info Area
+    const infoArea = document.querySelector(".villager-information")
+    const infoTitle = document.createElement("h3")
+    const infoBirthday = document.createElement("p")
+    const infoGender = document.createElement("p")
+    const infoHobby = document.createElement("p")
+    const infoPersonality = document.createElement("p")
+    const infoSaying = document.createElement("p")
+    const infoSpecies = document.createElement("p")
+    const infoCatchPhrase = document.createElement("p")
+
+    infoTitle.textContent = "Information"
+    infoTitle.style.textDecoration = "bold"
+    infoBirthday.textContent = `Date of birth: ${birthday}`
+    infoGender.textContent = `Gender: ${gender}`
+    infoSpecies.textContent = `Species: ${species}`
+    infoPersonality.textContent = `Personality: ${personality}`
+    infoHobby.textContent = `Hobby: ${hobby}`
+    infoCatchPhrase.textContent = `Catch-Phrase: ${catchPhrase}`
+    infoSaying.textContent = `${villagerName} says: ${saying}`
+
+    
+    infoArea.replaceChildren()
+    infoArea.append(infoTitle, infoBirthday, infoGender, infoSpecies, infoPersonality, infoHobby, infoCatchPhrase, infoSaying)
+}
--- scratch.md
+++ scratch.md
@@ -6,11 +6,11 @@ PHASE 1 PROJECT NOTES
 ## Requirements:
 
 1.[] HTML CSS JS
-2.[] Public API or db.json
-3.[] 2 distint event listener
-4.[] Iterating through arrays
+2.[x] Public API or db.json
+3.[x] 2 distint event listener
+4.[x] Iterating through arrays
 5.[] Use array methods
-6.[] Single page only
+6.[x] Single page only
 7.[] 30+ commits
 8.[] Render data in the HTML DOM
 9.[] 5 objects
